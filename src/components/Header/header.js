ba9736003587742bfee3100fe01e209c7aac0b43
moviesharing <109523229+moviesharing@users.noreply.github.com>
2022-07-23 16:18:34 +0800

    Add files via upload

---
+++ src/components/Header/header.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { Link } from "react-router-dom";
+const { Header } = Layout;
+
+import SearchComponent from '../Search/Search';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <Header>
+                <div className="logo"><Link to="/">Old Salt</Link></div>
+                <Menu
+                    theme="light"
+                    mode="horizontal"
+                    style={{ lineHeight: '64px', background: '#1890ff' }}
+
+                >
+                </Menu>
+                </Header>
+            </div>
+        )
+    }
+}
